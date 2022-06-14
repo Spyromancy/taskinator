16aa6c873e63cb8d09a88d27474ed58afd375255
Josh <josh.haddix598@gmail.com>
2022-06-14 16:21:38 -0500

    added edit, delete, and status dropdown to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var taskFormHandler = function(event) {
     event.preventDefault(); // keep the page from refreshing on form submit
@@ -20,6 +21,8 @@ var taskFormHandler = function(event) {
 var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li"); // create a new <li> element
     listItemEl.className = "task-item"; // <li class="task-item">
+    listItemEl.setAttribute("data-task-id",taskIdCounter); // gives the <li> element a data-task-id attribute with value set to taskIdCounter for identification purposes
+
     var taskInfoEl = document.createElement("div"); // create a new <div> element
     taskInfoEl.className = "task-info"; // <div class="task-info">
     taskInfoEl.innerHTML = `<h3 class='task-name'>${taskDataObj.name}</h3><span class='task-type'>${taskDataObj.type}</span>`
@@ -30,7 +33,49 @@ var createTaskEl = function(taskDataObj) {
     </div>
     */
     listItemEl.appendChild(taskInfoEl); // attach <div> to the <li> element
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    listItemEl.appendChild(taskActionsEl);
     tasksToDoEl.appendChild(listItemEl); // attach the <li> element to the <ul> element
+    
+    taskIdCounter++; // increment task counter for next unique id
 }
+var createTaskActions = function(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.classList= "task-actions";
+
+    //create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id",taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    // create delete button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id",taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    // create dropdown
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name","status-change");
+    statusSelectEl.setAttribute("data-task-id",taskId);
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+    for(var i =0; i<statusChoices.length; i++){
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value",statusChoices[i]);
+
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+
+    actionContainerEl.appendChild(statusSelectEl);
+
+    return actionContainerEl;
+};
 
 formEl.addEventListener("submit",taskFormHandler);
